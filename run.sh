63b5b543efeefb851b054d84fa9aba74bc5df975
Rich <r.fitzjohn@imperial.ac.uk>
2017-11-09 10:00:19 +0000

    Get key bits sorted out properly

--- run.sh
+++ run.sh
@@ -1,17 +1,32 @@
 #!/usr/bin/env bash
-VAULT_ADDR=https://support.montagu.dide.ic.ac.uk:8200
 
-REGISTRY_NAME=registry
+REGISTRY_PORT=5000
+REGISTRY_CONTAINER=registry
 REGISTRY_VOLUME_NAME=registry_data
-RUNNING=$(docker inspect --format="{{.State.Running}}" $REGISTRY_NAME 2> \
-                 /dev/null)
+REGISTRY_DOMAIN=docker.support.montagu.dide.ic.ac.uk
+
+## These should not be changed
+VAULT_ADDR=https://support.montagu.dide.ic.ac.uk:8200
 PATH_CRT=certs/domain.crt
 PATH_KEY=certs/domain.key
 
+RUNNING=$(docker inspect --format="{{.State.Running}}" $REGISTRY_CONTAINER 2> \
+                 /dev/null)
 
 if [[ "$RUNNING" == "true" ]]; then
     echo "Registry already running"
     exit 0
+fi
+
+cat certs/${REGISTRY_DOMAIN}.crt \
+    certs/QuoVadisOVIntermediateCertificate.crt > \
+    ${PATH_CRT}
+
+if [ ! -f $PATH_KEY ]; then
+    echo "Reading ssl key"
+    vault auth -method=github
+    vault read -field=key /secret/ssl/registry > $PATH_KEY
+    chmod 600 $PATH_KEY
 fi
 
 docker volume inspect $REGISTRY_VOLUME_NAME 2> /dev/null > /dev/null
@@ -23,18 +38,14 @@ else
     exit 1
 fi
 
-if [ ! -f $PATH_KEY ]; then
-    echo "Reading ssl key"
-    vault auth -method-github
-    vault read -field /secret/ssl/registry > $PATH_KEY
-    chmod 600 $PATH_KEY
-fi
-
 echo "Starting docker registry"
-docker run -d -p 5000:5000 --restart=always --name registry \
+docker run \
+       --name $REGISTRY_CONTAINER \
+       -d --restart=always \
+       -p ${REGISTRY_PORT}:5000 \
        -v `pwd`/certs:/certs \
        -v $REGISTRY_VOLUME_NAME:/var/lib/registry \
-       -e REGISTRY_HTTP_TLS_CERTIFICATE=$PATH_CRT \
-       -e REGISTRY_HTTP_TLS_KEY=$PATH_KEY \
+       -e REGISTRY_HTTP_TLS_CERTIFICATE=/$PATH_CRT \
+       -e REGISTRY_HTTP_TLS_KEY=/$PATH_KEY \
        registry:2
 exit $?
